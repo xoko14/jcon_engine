6b87dbe6c73d283acfce12687426ebf46d1e41a9
xoko14 <xoquinperezb@gmail.com>
2021-08-17 11:53:04 +0200

    render canvas placeholder

--- src/main/java/com/jcon_engine/Canvas.java
+++ src/main/java/com/jcon_engine/Canvas.java
@@ -1,16 +1,23 @@
 package com.jcon_engine;
 
 import com.jcon_engine.objects2D.Object2D;
+import java.util.ArrayList;
 
 public class Canvas {
     private int x;
     private int y;
+    private ArrayList<Object2D> shapes;
+    private char[][] canvas;
 
     public Canvas(int x, int y){
         this.x = x;
         this.y = y;
+        shapes = new ArrayList<>();
     }
 
+    public char[][] getCanvas(){
+        return canvas;
+    }
 
     
 }
